fcb07dcde7a8d2981ee14885740ae4599d67f646
kkr0787 <kkr0787@gmail.com>
2023-04-14 21:09:06 +0900

    initial commit

---
+++ src/main/java/shop/readmecorp/adminserverreadme/AdminServerReadMeApplication.java
@@ -0,0 +1,13 @@
+package shop.readmecorp.adminserverreadme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServerReadMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerReadMeApplication.class, args);
+	}
+
+}
